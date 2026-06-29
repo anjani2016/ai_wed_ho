1989301ad99c2dc62e2417e5ec8eee773ed26c74
anjani duddukuru <anjani.duddukuru@gmail.com>
2026-06-28 22:38:06 -0400

    feat(frontend): add proxy rewrite to EC2 to bypass mixed content policy

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/ec2/:path*',
+        destination: 'http://3.14.150.6:8000/:path*',
+      },
+    ]
+  },
 }
 
 export default nextConfig
